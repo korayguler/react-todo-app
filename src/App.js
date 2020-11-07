7bfce22451f7cf6cf0326aa718b6feb1bb72f98c
korayguler <tr.koray.guler@gmail.com>
2020-11-07 04:43:31 +0300

    :sparkles: new feature: add todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,108 @@
+import React, { useState, useEffect } from 'react';
+
 function App() {
-  return <div className='App'></div>;
+  const [todos, setTodos] = useState([]);
+  const url = 'http://localhost:3000/todos';
+
+  useEffect(() => {
+    get(url).then((data) => setTodos(data));
+  });
+  function add(e) {
+    e.preventDefault();
+    const todo = e.target.todo;
+    const level = e.target.level;
+    if (todo.value === '' || level.value === '') return;
+
+    post(url, {
+      id: todos.length + 1,
+      todo: todo.value,
+      level: level.value,
+      check: false,
+    }).then((data) => console.log(data));
+  }
+
+  async function post(url = '', data = '') {
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    return response.json();
+  }
+
+  async function get(url = '') {
+    const response = await fetch(url);
+    return response.json();
+  }
+
+  function setCheck(e) {
+    console.log(e);
+  }
+
+  return (
+    <div className='container'>
+      <h4>React Todo App</h4>
+      <div className='row'>
+        <div className='five column'>
+          <form onSubmit={add}>
+            <label htmlFor='todo'>your todo</label>
+            <textarea
+              className='u-full-width'
+              placeholder='todo …'
+              id='todo'
+              style={{ resize: 'none' }}
+            ></textarea>
+
+            <div className='row'>
+              <select className='three columns ' id='level'>
+                <option value='normal'>Normal</option>
+                <option value='important'>Önemli</option>
+                <option value='less-important'>Az önemli</option>
+              </select>
+              <input
+                className='three columns button-primary u-pull-right'
+                type='submit'
+              ></input>
+              <button className='button u-pull-right'>Clear All</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <table className='u-full-width todos'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Todo</th>
+            <th>Level</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((data) => {
+            return (
+              <tr key={data.id} className={data.level}>
+                <td>{data.id}</td>
+                <td>{data.todo}</td>
+                <td>{data.level}</td>
+                <td>
+                  <input
+                    type='checkbox'
+                    defaultChecked={data.check}
+                    onChange={setCheck}
+                  />
+                </td>
+                <tr></tr>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './skeleton.css';
 import './index.css';
+
 import App from './App';
 
 ReactDOM.render(
@@ -9,7 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
